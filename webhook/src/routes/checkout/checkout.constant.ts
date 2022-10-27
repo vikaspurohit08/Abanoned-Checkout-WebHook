52801881e2a23a24bbeb90036c20f683b33786dc
Vikas Purohit <vikaspurohit08@gmail.com>
2022-10-27 19:04:28 +0530

    fix gitIgnore files

--- webhook/src/routes/checkout/checkout.constant.ts
+++ webhook/src/routes/checkout/checkout.constant.ts
@@ -2,11 +2,11 @@ export const scheduleAddTime = [
   {
     title:
       "Looks like you missed to complete your order. Continue where you left off",
-    addTime: 5 * 60 * 1000,
+    addTime: 5 * 1000,
   },
   {
     title: "Hey, where are you? Your order is ready to get completed",
-    addTime: 24 * 60 * 60 * 1000,
+    addTime: 60 * 1000,
   },
   {
     title: "Hurry!!! Complete your order before it gets out of sold",
